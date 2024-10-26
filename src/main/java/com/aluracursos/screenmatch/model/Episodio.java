03073f79411d2547e36d05c17bb2e7b08424a6a3
julianycia.m@gmail.com <julianycia.m@gmail.com>
2024-10-26 14:20:15 -0300

    agregando funciones

---
+++ src/main/java/com/aluracursos/screenmatch/model/Episodio.java
@@ -0,0 +1,80 @@
+package com.aluracursos.screenmatch.model;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeParseException;
+
+public class Episodio {
+    private Integer temporada;
+    private String titulo;
+    private Integer numeroEpisodio;
+    private Double evalucion;
+    private LocalDate fechaDeLanzamiento;
+
+    public Episodio(Integer numero, DatosEpisodio d) {
+        this.temporada = numero;
+        this.titulo = d.titulo();
+        this.numeroEpisodio = d.numeroDeEpisodio();
+        try{
+            this.evalucion = Double.valueOf(d.evaluacion());
+        } catch (NumberFormatException e){
+            this.evalucion = 0.0;
+        }
+        try{
+            this.fechaDeLanzamiento = LocalDate.parse(d.fechaDeLanzamiento());
+        }catch (DateTimeParseException e){
+            this.fechaDeLanzamiento = null;
+        }
+
+
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public Integer getTemporada() {
+        return temporada;
+    }
+
+    public void setTemporada(Integer temporada) {
+        this.temporada = temporada;
+    }
+
+    public Integer getNumeroEpisodio() {
+        return numeroEpisodio;
+    }
+
+    public void setNumeroEpisodio(Integer numeroEpisodio) {
+        this.numeroEpisodio = numeroEpisodio;
+    }
+
+    public Double getEvalucion() {
+        return evalucion;
+    }
+
+    public void setEvalucion(Double evalucion) {
+        this.evalucion = evalucion;
+    }
+
+    public LocalDate getFechaDeLanzamiento() {
+        return fechaDeLanzamiento;
+    }
+
+    public void setFechaDeLanzamiento(LocalDate fechaDeLanzamiento) {
+        this.fechaDeLanzamiento = fechaDeLanzamiento;
+    }
+
+    @Override
+    public String toString() {
+        return
+                "temporada=" + temporada +
+                ", titulo='" + titulo + '\'' +
+                ", numeroEpisodio=" + numeroEpisodio +
+                ", evalucion=" + evalucion +
+                ", fechaDeLanzamiento=" + fechaDeLanzamiento;
+    }
+}
